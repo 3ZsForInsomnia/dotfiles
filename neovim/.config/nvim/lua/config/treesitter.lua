1b0f27cdbc822d9cb7a885de2d73a027b734f397
Zach Levine <zach.levine@addepar.com>
2022-12-26 17:55:07 -0500

    Temp commit before retrying lazy - need to limit treesitter initial load

--- neovim/.config/nvim/lua/config/treesitter.lua
+++ neovim/.config/nvim/lua/config/treesitter.lua
@@ -1,5 +1,5 @@
 require 'nvim-treesitter.configs'.setup {
-  ensure_installed = "all", -- one of "all", "language", or a list of languages
+  ensure_installed = "javascript", -- one of "all", "language", or a list of languages
   sync_install = true,
   auto_install = true,
   highlight = {
